e52e259ebaaf627d7696f3b847f2e14e14d06254
Luuk Stassen <lhc.stassen@student.avans.nl>
2023-11-29 13:45:12 +0100

    Added Main class with possible fix for startup, testing required

--- src/RobotMain.java
+++ src/RobotMain.java
@@ -1,21 +1,57 @@
 import TI.BoeBot;
 import TI.PinMode;
 
-public class RobotMain {
-    public static void main(String[] args) {
-        Zoomer zoomer = new Zoomer(12, 14);
-        EmergencyStop emergencyStop = new EmergencyStop(0);
-        
-        while (true) {
+import java.awt.desktop.AboutEvent;
 
-            //zoomer.update(12);
-            zoomer.update(14);
-            if(emergencyStop.check()){
-                //check for emergency stop press, stop the wheels and break the loop
-                MotorAansturen.stop();
-                break;
-            }
-            BoeBot.wait(1);
+public class RobotMain {
+
+    private Boolean isRunning;
+    private Zoomer zoomer;
+
+    public EmergencyStop emergencyStop;
+    RobotMain() {
+        this.startUp();
+        this.zoomer = new Zoomer(10, 11);
+        this.emergencyStop = new EmergencyStop(0);
+    };
+
+    public void startUp() {
+        this.isRunning = true;
+    }
+
+    public void update() {
+        if (this.emergencyStop.check()){
+            MotorAansturen.stop();
+            this.isRunning = false;
+            return;
         }
+
+        zoomer.update(10);
+        zoomer.update(11);
+    }
+
+    public Boolean getRunning() {
+        return isRunning;
+    }
+
+    public Zoomer getZoomer() {
+        return zoomer;
     }
 }
+
+
+//    public static void main(String[] args) {
+//        Zoomer zoomer = new Zoomer(12, 14);
+//        EmergencyStop emergencyStop = new EmergencyStop(0);
+//
+//        while (true) {
+//
+//            //zoomer.update(12);
+//            zoomer.update(14);
+//            if(emergencyStop.check()){
+//                //check for emergency stop press, stop the wheels and break the loop
+//                MotorAansturen.stop();
+//                break;
+//            }
+//            BoeBot.wait(1);
+//        }
