868eea8e1f8909d5edcbf1087831c9bee08d42e9
spookdude <mmaa.woestenburg@student.avans.nl>
2023-12-07 16:49:47 +0100

    15 V1.2 neopixel short version + single pixel setter

--- src/actuators/NeoPixel.java
+++ src/actuators/NeoPixel.java
@@ -1,7 +1,52 @@
 package actuators;
 
+import TI.BoeBot;
+import java.awt.*;
+import static java.awt.Color.*;
+import  TI.Timer;
+
 public class NeoPixel {
+    private Color colour;
+    private int pixel;
+    private boolean state;
+    private Timer time;
+    private boolean blink=true;
+
     public NeoPixel(){
+        this.time = new Timer(1);
+    }
+    public void lights(){time.setInterval(500);
+        if (state) {
+            if (!time.timeout()){
+                for (int i=0;i<=5;i++){
+                    if ((i%2==0)==blink){BoeBot.rgbSet(i,black);}
+                    else {BoeBot.rgbSet(i,colour);}
+                }
+                blink = !blink;
+                BoeBot.rgbShow();
+            }
+        }
+        for (int i =0;i<=5;i++){
+            BoeBot.rgbSet(i, black);
+        }
+        BoeBot.rgbShow();
+    }
+    public void pix(){
+        for (int i=0;i<=5;i++){BoeBot.rgbSet(i,black);}
+        BoeBot.rgbSet(pixel,colour);
+        BoeBot.rgbShow();
+    }
+    public void setNeoPixel(Color colour, boolean state){
+        this.colour = colour;
+        this.state = state;
+    }
+    public Color getNeoPixelColor(){return this.colour;}
+    public void setSingleNeoPixelPin(int pixel,Color colour){
+        this.pixel = pixel;
+        this.colour = colour;
+    }
+    public int getPixel(){return  this.pixel;}
+    public boolean getNeoPixelState() {return this.state;}
 
     }
 }
