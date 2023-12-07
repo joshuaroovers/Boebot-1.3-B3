f1a5aa0f7f43876be0fd71399f370db1f0a3acb7
spookdude <mmaa.woestenburg@student.avans.nl>
2023-12-07 16:49:47 +0100

    15 V1.2 neopixel short version + single pixel setter

--- src/actuators/NeoPixel.java
+++ src/actuators/NeoPixel.java
@@ -1,35 +1,28 @@
 package actuators;
 
 import TI.BoeBot;
-import TI.Timer;
-
 import java.awt.*;
-
-import static java.awt.Color.black;
+import static java.awt.Color.*;
+import  TI.Timer;
 
 public class NeoPixel {
     private Color colour;
-    //    private int pixel;
+    private int pixel;
     private boolean state;
     private Timer time;
+    private boolean blink=true;
 
     public NeoPixel(){
         this.time = new Timer(1);
     }
     public void lights(){time.setInterval(500);
         if (state) {
-            while (!time.timeout()){
+            if (!time.timeout()){
                 for (int i=0;i<=5;i++){
-                    if (i%2==0){BoeBot.rgbSet(i,black);}
+                    if ((i%2==0)==blink){BoeBot.rgbSet(i,black);}
                     else {BoeBot.rgbSet(i,colour);}
                 }
-                BoeBot.rgbShow();
-            }
-            while (!time.timeout()){
-                for (int i=0;i<=5;i++){
-                    if (i%2==1){BoeBot.rgbSet(i,black);}
-                    else {BoeBot.rgbSet(i,colour);}
-                }
+                blink = !blink;
                 BoeBot.rgbShow();
             }
         }
@@ -38,13 +31,21 @@ public class NeoPixel {
         }
         BoeBot.rgbShow();
     }
+    public void pix(){
+        for (int i=0;i<=5;i++){BoeBot.rgbSet(i,black);}
+        BoeBot.rgbSet(pixel,colour);
+        BoeBot.rgbShow();
+    }
     public void setNeoPixel(Color colour, boolean state){
         this.colour = colour;
-        this.state=state;
+        this.state = state;
     }
     public Color getNeoPixelColor(){return this.colour;}
-    //    public int getNeoPixelPin(){return this.pixel;}
+    public void setSingleNeoPixelPin(int pixel,Color colour){
+        this.pixel = pixel;
+        this.colour = colour;
+    }
+    public int getPixel(){return  this.pixel;}
     public boolean getNeoPixelState() {return this.state;}
 
 }
-
