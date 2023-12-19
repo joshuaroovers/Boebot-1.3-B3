185e37696f34cfd0b2a64d1b4ea48b3e40ea8456
spookdude <mmaa.woestenburg@student.avans.nl>
2023-12-19 16:51:02 +0100

    19_splitter v1.0 added splitter
    Added splitter

--- src/head/MotorHelper.java
+++ src/head/MotorHelper.java
@@ -2,6 +2,10 @@ package head;
 
 import actuators.Motor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class MotorHelper {
     private Motor s1;
     private Motor s2;
@@ -9,6 +13,25 @@ public class MotorHelper {
     public MotorHelper(Motor s1, Motor s2) {
         this.s1 = s1;
         this.s2 = s2;
+    }
+
+    /**
+     * Splitter
+     * @author Morris Woestenburg
+     * @param command
+     * splits the command from the GUI into individual characters that call on the motor methods
+     */
+    public void Splitter(String command){
+        String splice = command;
+        ArrayList<String> split = new ArrayList<>();
+        split.add(String.valueOf(splice.charAt(0)));
+        for (int i=0;i<command.length();i++) {
+            String control = split.get(i);
+            if (Objects.equals(control, "l")){turn_left();}
+            if (Objects.equals(control, "r")){turn_right();}
+            if (Objects.equals(control, "v")){forwards();}
+            if (Objects.equals(control, "e,")){stop();}
+        }
     }
 
     public void Wheels(int Wheel1, int Wheel2){
