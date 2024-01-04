40946a71e3511adb4f9e76b4175751ff664c7e52
spookdude <mmaa.woestenburg@student.avans.nl>
2024-01-04 15:48:22 +0100

    19 V1.0 Splitter
    werkt maar moet nog verbonden worden met de lijnvolger

--- src/head/Splitter.java
+++ src/head/Splitter.java
@@ -2,6 +2,7 @@ package head;
 
 import actuators.Claw;
 import actuators.Motor;
+import sensors.LineDetector;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -11,6 +12,10 @@ public class Splitter implements Updateable{
     private  String splice;
     private ArrayList<String> split;
     private String control;
+    private MotorHelper motorHelper;
+    private Claw claw;
+    private int step;
+    private LineDetector lineDetector;
     public Splitter(){
     }
 
@@ -25,13 +30,14 @@ public class Splitter implements Updateable{
 //            split.add(String.valueOf(splice.charAt(splice.length()-1)));
 //            String control = split.get(0);
 //            split.remove(split.get(0));
-//            if (Objects.equals(control, "r")){MotorHelper.turn_left();}
-//            if (Objects.equals(control, "l")){MotorHelper.turn_right();}
-//            if (Objects.equals(control, "v")){MotorHelper.forwards();}
-//            if (Objects.equals(control, "n")){Motor.hardStop();}
-//            if (Objects.equals(control, "e,")){MotorHelper.stop();}
-//            if (Objects.equals(control, "o")){Claw.open();}
-//            if (Objects.equals(control, "c")){Claw.close();}
+
+//            if (Objects.equals(control, "r")){motorHelper.turn_left();}
+//            if (Objects.equals(control, "l")){motorHelper.turn_right();}
+//            if (Objects.equals(control, "v")){motorHelper.forwards();}
+//            if (Objects.equals(control, "n")){motorHelper.hardStop();}
+//            if (Objects.equals(control, "e,")){motorHelper.stop();}
+//            if (Objects.equals(control, "o")){claw.open();}
+//            if (Objects.equals(control, "c")){claw.close();}
 //        }
 //    }
     /**
@@ -39,20 +45,29 @@ public class Splitter implements Updateable{
      * @author Morris Woestenburg
      * splits the command from the GUI into individual characters that call on the motor methods
      */
-    @Override
-    public void update() {
-        if (splice.isEmpty()){return;}
+    private void splitter(){
         for (int i=0;i<splice.length();i++) {
+
             control = String.valueOf(splice.charAt(i));
-            if (Objects.equals(control, "l")){MotorHelper.turn_left();}
-            if (Objects.equals(control, "r")){MotorHelper.turn_right();}
-            if (Objects.equals(control, "v")){MotorHelper.forwards();}
-            if (Objects.equals(control, "n")){Motor.hardStop();}
-            if (Objects.equals(control, "e,")){MotorHelper.stop();}
-            if (Objects.equals(control, "o")){Claw.open();}
-            if (Objects.equals(control, "c")){Claw.close();}
-//            if (Objects.equals(control, "b") && String.valueOf(splice.charAt(splice.length() - 1)).equals("b") && (control + 1).isEmpty()){returning(splice);}
-//            else{continue;}
+            decoder(control);
         }
     }
+    private void decoder(String control){
+//        if (splice.isEmpty()){return;}
+        if (Objects.equals(control, "l")){motorHelper.turn_left();}
+        if (Objects.equals(control, "r")){motorHelper.turn_right();}
+        if (Objects.equals(control, "v")){motorHelper.forwards();}
+        if (Objects.equals(control, "n")){motorHelper.hardStop();}
+        if (Objects.equals(control, "e,")){motorHelper.stop();}
+        if (Objects.equals(control, "o")){claw.open();}
+        if (Objects.equals(control, "c")){claw.close();}
+//            if (Objects.equals(control, "b") && String.valueOf(splice.charAt(splice.length() - 1)).equals("b") && (control + 1).isEmpty()){returning(splice);}
+//            else{continue;}
+    }
+
+    @Override
+    public void update() {
+
+    }
 }
+
